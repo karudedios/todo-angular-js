5665b42ebb951315c475304862fa820fff3c2ef1
Carlos De Dios <c.dedios@outlook.com>
2016-05-31 23:54:14 +0000

    Adding gulp deps and tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,2 +1,24 @@
-const gulp = require('gulp');
-const copy = require('gulp-copy');
+"use strict";
+
+const path  = require('path');
+const gulp  = require('gulp');
+const env   = require('gulp-env');
+const mocha = require('gulp-mocha');
+
+const specSrc = path.join('.', 'specs', '**', '*-spec.js');
+
+gulp.task('run:spec', () => {
+  const mochaRunner = mocha({ reporter: 'dot' });
+  const environment = env({ file: '.config-spec.json' });
+    
+  return gulp.src(specSrc)
+    .pipe(environment)
+    .pipe(mochaRunner)
+    .pipe(environment.reset);
+});
+
+gulp.task('watch:spec', () => {
+  return gulp.watch(specSrc, ['run:spec']);
+});
+    
+gulp.task('default', ['run:spec']);
