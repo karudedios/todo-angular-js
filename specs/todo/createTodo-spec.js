e34d8cb891f04bd404259ff22a29b3311e66a106
Carlos De Dios <c.dedios@outlook.com>
2016-05-31 19:16:11 +0000

    Updating specs ignored and package.json

---
+++ specs/todo/createTodo-spec.js
@@ -0,0 +1,24 @@
+const chai          = require('chai');
+const DatabaseMock  = require('../helpers/databaseMock');
+const Todo          = require('../../features/todo/model/todo');
+const CreateTodo    =  require('../../features/todo/services/createTodo');
+
+describe("create todo", () => {
+  before(() => {
+    chai.should();
+  });
+  
+  it("should ask for the dependencies it needs", () => {
+    CreateTodo.length.should.equal(1);
+    (() => new CreateTodo(Todo)).should.not.throw();
+  });
+  
+  it("should perform the creation when .create is called", () => {
+    return new CreateTodo(Todo)
+      .create({ name: "Write a book" })
+      .then(todo => {
+        todo._id.should.be.defined();
+        todo.name.should.equal("Write a book");
+      });
+  });
+});
