51ccbacce9b8f9903792097e110098b5f8cb1297
Carlos De Dios <c.dedios@outlook.com>
2016-06-01 00:23:35 +0000

    Creating todo from createTodo-spec

--- features/todo/model/todo.js
+++ features/todo/model/todo.js
@@ -1,1 +1,11 @@
-module.exports = {};
+const mongoose  = require('mongoose');
+const Schema    = mongoose.Schema;
+
+const TodoSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Todo', TodoSchema);
--- specs/todo/createTodo-spec.js
+++ specs/todo/createTodo-spec.js
@@ -3,23 +3,47 @@ const DatabaseMock  = require('../helpers/databaseMock');
 const Todo          = require('../../features/todo/model/todo');
 const CreateTodo    =  require('../../features/todo/services/createTodo');
 
-describe("create todo", () => {
-  before((done) => {
-    new DatabaseMock().connect(done);
+describe('create todo', () => {
+  const db = new DatabaseMock();
+  
+  before(() => {
     chai.should();
+    db.connect();
   });
   
-  it("should ask for the dependencies it needs", () => {
+  after((done) => {
+    db.clearDb(done);
+  });
+  
+  it('should ask for the dependencies it needs', () => {
     CreateTodo.length.should.equal(1);
     (() => new CreateTodo(Todo)).should.not.throw();
   });
   
-  it("should perform the creation when .create is called", () => {
+  it('should perform the creation when .create is called', () => {
+    const name = 'Write a book';
+    
     return new CreateTodo(Todo)
-      .create({ name: "Write a book" })
+      .create({ name })
       .then(todo => {
-        todo._id.should.be.defined();
-        todo.name.should.equal("Write a book");
+        todo._id.should.be.defined;
+        todo.name.should.equal(name);
+      });
+  });
+  
+  it('should gracefully fail if required fields are invalid', () => {
+    return new CreateTodo(Todo)
+      .create({ name: 5 })
+      .catch(err => {
+        err.message.should.contain('"todo.name" must be a string');
+      });
+  });
+  
+  it('should gracefully fail if required fields are not provided', () => {
+    return new CreateTodo(Todo)
+      .create({ })
+      .catch(err => {
+        err.message.should.contain('"todo.name" is required');
       });
   });
 });
