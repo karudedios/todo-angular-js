60bfe58b0fc9305642669bb8234de5e0d1e737d8
Carlos De Dios <c.dedios@outlook.com>
2016-06-01 00:23:44 +0000

    Making database mock

--- specs/helpers/databaseMock.js
+++ specs/helpers/databaseMock.js
@@ -1,21 +1,27 @@
+const mongoose = require('mongoose');
+
 module.exports = class DatabaseMock {
-  constructor() {
-    
+  constructor(connectionUrl = process.env.SPEC_MONGO_URL) {
+    Object.assign(this, { connectionUrl });
   }
   
   connect() {
+    if(mongoose.connection.db && mongoose.connection.readyState) {
+      return;
+    }
     
+    mongoose.connect(this.connectionUrl);
   }
   
   disconnect() {
-    
+    mongoose.disconnect();
   }
   
-  clearCollection() {
-    
+  clearCollection(collection, cb) {
+    mongoose.connection.db.clearCollection(collection, cb);
   }
   
-  clearCollections() {
-    
+  clearDb(cb) {
+    mongoose.connection.db.dropDatabase(cb);
   }
 };
