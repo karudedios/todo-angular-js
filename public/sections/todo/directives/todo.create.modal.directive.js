62b5e9096aa9232fa422f40e1d27528e808d113b
Kenji Mukai <kesalcedo@arshumano.com>
2016-06-03 15:38:32 -0400

    adding modal to create todo

---
+++ public/sections/todo/directives/todo.create.modal.directive.js
@@ -0,0 +1,29 @@
+angular.module('todoApp')
+    .directive('myTodoCreateModal', myTodoCreateModal)
+    .directive('myTodoCreateModalBtn', myTodoCreateModalBtn);
+
+function myTodoCreateModal(){
+    return {
+        scope : {
+            todoModalId : '@'
+        },
+        templateUrl : 'sections/todo/directives/todo.create.modal.html'
+    }
+}
+
+function myTodoCreateModalBtn(){
+    return {
+        scope : {
+            todoModalId : '@',
+            classes : '@?'
+        },
+        transclude : true,
+        templateUrl : 'sections/todo/directives/todo.create.modal.btn.html',
+        controller : function($scope){
+            $scope.openModal = function(){
+                console.log($scope.todoModalId)
+                $('#' + $scope.todoModalId).openModal();
+            }
+        }
+    }
+}
