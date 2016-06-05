aefa53e288f9f59421b95bc792ed8a7fc85c87f6
Carlos De Dios <c.dedios@outlook.com>
2016-06-05 06:10:10 +0000

    Trying to unfuck this

--- features/user/router/user.js
+++ features/user/router/user.js
@@ -1,7 +1,6 @@
 const UserDto           = require('../model/dto');
 const Router            = require('express').Router;
 const FindUser          = require('../services/findUser');
-const CreateUser        = require('../services/createUser');
 const UpdateUser        = require('../services/updateUser');
 const PromisedResponse  = require('../../../utils/promisedResponse');
 
@@ -10,16 +9,6 @@ module.exports = (User) =>
     .get('/:id', PromisedResponse(req =>
       new FindUser(User)
         .findOne({ _id: req.params.id })
-        .then(UserDto.new)))
-
-    .post('/', PromisedResponse(req =>
-      new FindUser(User)
-        .findOne({ usename: req.params.username })
-        .then(user => {
-          if (user) throw "Username exists";
-          return req.body;
-        })
-        .then(new CreateUser(User).create)
         .then(UserDto.new)))
 
     .put('/:id', PromisedResponse(req =>
